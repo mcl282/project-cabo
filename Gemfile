87adbf5ad52511d00aa661e8b0b887f75ff9468b
Michael <mcl282@gmail.com>
2017-04-14 22:08:58 +0000

    install active admin

--- Gemfile
+++ Gemfile
@@ -44,4 +44,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-#test
+# ActiveAdmin
+gem 'devise', '~> 4.2'
+gem 'activeadmin', github: 'activeadmin'
+gem 'inherited_resources', git: 'https://github.com/activeadmin/inherited_resources'
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,11 @@ module CaboServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #the following is setup for ActiveAdmin    
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
