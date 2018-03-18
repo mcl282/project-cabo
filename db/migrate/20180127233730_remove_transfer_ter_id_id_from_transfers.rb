7c07b83c9e1460fed81a2b5688faca0075401add
Michael <mcl282@gmail.com>
2018-03-18 00:03:47 +0000

    transfer term id renaming

--- db/migrate/20180127233730_remove_transfer_ter_id_id_from_transfers.rb
+++ db/migrate/20180127233730_remove_transfer_ter_id_id_from_transfers.rb
@@ -1,5 +1,5 @@
 class RemoveTransferTerIdIdFromTransfers < ActiveRecord::Migration[5.0]
   def change
-    remove_column :transfers, :transfer_term_id_id, :integer
+    remove_column :transfers, :transfer_term_id, :integer
   end
 end
