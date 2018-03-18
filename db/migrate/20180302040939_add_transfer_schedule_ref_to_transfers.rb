af9cca66e76196add6837e1e3d306505744a02cb
Michael <mcl282@gmail.com>
2018-03-18 00:09:02 +0000

    fix error in migration AddTransferScheduleRefToTransfers

--- db/migrate/20180302040939_add_transfer_schedule_ref_to_transfers.rb
+++ db/migrate/20180302040939_add_transfer_schedule_ref_to_transfers.rb
@@ -1,5 +1,5 @@
 class AddTransferScheduleRefToTransfers < ActiveRecord::Migration[5.0]
   def change
-    add_reference :transfers, :transfer_schedule_id, foreign_key: true
+    add_reference :transfers, :transfer_schedule, foreign_key: true
   end
 end
