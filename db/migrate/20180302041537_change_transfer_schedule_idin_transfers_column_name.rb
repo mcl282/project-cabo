65fe4abb3139b7bb010cd629361d1d1c36647db0
Michael <mcl282@gmail.com>
2018-03-18 00:11:18 +0000

    update migration ChangeTransferScheduleIdinTransfersColumnName

--- db/migrate/20180302041537_change_transfer_schedule_idin_transfers_column_name.rb
+++ db/migrate/20180302041537_change_transfer_schedule_idin_transfers_column_name.rb
@@ -1,5 +1,5 @@
 class ChangeTransferScheduleIdinTransfersColumnName < ActiveRecord::Migration[5.0]
   def change
-    rename_column :transfers, :transfer_schedule_id_id, :transfer_schedule_id
+    rename_column :transfers, :transfer_schedule_id, :transfer_schedule_id
   end
 end
